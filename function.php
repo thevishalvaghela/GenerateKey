d20b9ca6a30e32f574cf721f8862956715fef04e
Vishal Vaghela <vishalsbaghela@gmail.com>
2020-05-07 21:44:51 +0530

    Create function.php

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+
+function generateKey($minlength = 20, $maxlength = 20, $uselower = true, $useupper = true, $usenumbers = true, $usespecial = false) {
+    $charset = '';
+    if ($uselower) {
+        $charset .= "abcdefghijklmnopqrstuvwxyz";
+    }
+    if ($useupper) {
+        $charset .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if ($usenumbers) {
+        $charset .= "123456789";
+    }
+    if ($usespecial) {
+        $charset .= "~@#$%^*()_+-={}|][";
+    }
+    if ($minlength > $maxlength) {
+        $length = mt_rand($maxlength, $minlength);
+    } else {
+        $length = mt_rand($minlength, $maxlength);
+    }
+    $key = '';
+    for ($i = 0; $i < $length; $i++) {
+        $key .= $charset[(mt_rand(0, strlen($charset) - 1))];
+    }
+    return $key;
+}
+
+?>
